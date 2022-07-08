bcf0a9539501c13f9b3dd294fc81d5c7e06912f8
hiuhihi78 <leduchieu2001x@gmail.com>
2022-07-08 10:12:27 +0700

    commit-day-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  user = {
+    name: 'haha',
+    age: '19',
+  };
+  twoWayDataBinding = 'hieuldhe150703';
+
+  //DATA BINDING//
+  // proproty binding
+  inputType: 'text';
+  inputValue: 'data binding';
+  // event binding
+  handler(event) {
+    console.log(event);
+  }
 }
